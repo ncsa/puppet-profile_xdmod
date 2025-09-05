62e51d8c50352d0626c83e5ad5002a645d687fd2
Ben Sperry <bsper2@illinois.edu>
2025-09-05 14:37:58 -0500

    SVCPLAN-6997 Add CI/CD and initial init

---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+# @summary Empty init class
+#
+# This class should never be included. Depending on the role,
+# include one or more of the following:
+# - Node generating xdmod reports: `include profile_xdmod::client`
+# - Node running xdmod service: `include profile_xdmod::server`
+#
+# @example
+#   include profile_xdmod
+class profile_xdmod {
+  # THIS CLASS IS INTENTIONALLY LEFT EMPTY
+
+  $notify_text = @("EOT"/)
+    The top level profile_backup class should not be used.
+    Instead use one of the following classes:
+      - profile_backup::client
+      - profile_backup::server
+    | EOT
+  notify { $notify_text:
+    withpath => true,
+    loglevel => 'warning',
+  }
+}
